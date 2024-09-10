4434c355bd140ee0e744b41d878cdc565b6ca399
Håkon Olsen <hakdo@outlook.com>
2024-09-10 11:29:01 +0200

    added admin api key

--- admin.php
+++ admin.php
@@ -16,11 +16,15 @@
             header('HTTP/1.1 403 Forbidden');
             exit();
         }
+        $comparekey = file_get_contents('apikeys.txt');
 
         // If the user is an admin, display the admin dashboard content
         ?>
 
         <h1>Welcome, Admin!</h1>
+        <p>Your API Key is: 
+            <?php echo $comparekey; ?>
+        </p>
 
         </div>
 </body>
